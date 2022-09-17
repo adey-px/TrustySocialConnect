4f4ae8cb95d54e1e0995ef8fcd88c17c718525eb
adey-px <aosteph@gmail.com>
2022-09-16 21:38:28 -0500

    Removed unnecessary files from template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Discord Webclone</h1>
+    </div>
+  );
+}
+
+export default App;
